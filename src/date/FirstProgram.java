404e13a1d57a1ced41f49a0cf809e88890384fb8
Taras Chornyi <taras.chornyi.v@gmail.com>
2022-08-16 17:48:20 +0300

    add package date

---
+++ src/date/FirstProgram.java
@@ -0,0 +1,18 @@
+package date;
+
+import java.time.LocalDate;
+
+public class FirstProgram {
+    /** @param hello */
+    static void infoDate(String hello) {
+        String txtDate = new String("Today's date: ");
+        LocalDate date = LocalDate.now();
+        System.out.println(hello + txtDate + date);
+    }
+    /** @param args */
+    public static void main(String...args) {
+        String txtHello = new String("Hello, World. ");
+        // System.out.println(args[0]);
+        infoDate(txtHello);
+    }
+}
