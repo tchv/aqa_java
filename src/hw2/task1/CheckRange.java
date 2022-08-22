cdfd6b4add24d566037397fb953610c7029dbd48
Taras Chornyi <taras.chornyi.v@gmail.com>
2022-08-22 15:39:25 +0300

    Add Check Range

--- src/hw2/task1/CheckRange.java
+++ src/hw2/task1/CheckRange.java
@@ -1,34 +1,42 @@
-package hw2.range;
+package hw2.task1;
 
 import java.util.Scanner;
 
 public class CheckRange {
     public static float getX(float min, float max, float num) {
         if (num >= min && num <= max) {
-            System.out.println("\nYes:");
+            System.out.println("\nYes");
         } else {
-            System.out.println("\nNo:");
+            System.out.println("\nNo");
         }
         return num;
     }
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         float min = -5f;
         float max = 5f;
 
-        Scanner sc = new Scanner(System.in);
+        System.out.println("-<*>- Checking whether numbers are in the range from [" + min + "] to [" + max + "] -<*>-");
+
+
+        System.out.println("\nPlease enter the first float and then press Return:");
         float num1 = sc.nextFloat();
+        System.out.println(">>> " + num1);
+
+        System.out.println("\nPlease enter the second float and then press Return:");
         float num2 = sc.nextFloat();
+        System.out.println(">>> " + num2);
+
+        System.out.println("\nPlease enter the third float and then press Return:");
         float num3 = sc.nextFloat();
+        System.out.println(">>> " + num3);
 
-        System.out.println(getX(min, max, num1));
-        System.out.println(getX(min, max, num2));
-        System.out.println(getX(min, max, num3));
-
-        System.out.println("\nMin:");
-        System.out.println(Math.min(Math.min(num1, num2), num3));
-        System.out.println("\nMax:");
-        System.out.println(Math.max(Math.max(num1, num2), num3));
+        System.out.println("\n[  *  RESULT  *  ]");
+        System.out.println(">>> " + getX(min, max, num1));
+        System.out.println(">>> " + getX(min, max, num2));
+        System.out.println(">>> " + getX(min, max, num3));
     }
 }
 
