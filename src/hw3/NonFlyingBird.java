2786bafdbd8748a1f50ae8229e751ed2d88d3137
Taras Chornyi <taras.chornyi.v@gmail.com>
2022-08-25 15:35:01 +0300

    Add hw3/NonFlyingBird

--- src/hw3/NonFlyingBird.java
+++ src/hw3/NonFlyingBird.java
@@ -1,2 +1,15 @@
-package hw3;public class NonFlyingBird {
+package hw3;
+
+public class NonFlyingBird extends Bird {
+    protected String protectMethod() {
+        return super.protectMethod();
+    }
+
+    public static void main(String[] args) {
+        NonFlyingBird nfb = new NonFlyingBird();
+        Bird[] b = {nfb};
+        for (Bird element: b) {
+            System.out.println(element.publicMethod());
+        }
+    }
 }
