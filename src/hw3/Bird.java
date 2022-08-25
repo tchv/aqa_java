18f6fe8ad81c567bf9e56a39e94193015fe4695e
Taras Chornyi <taras.chornyi.v@gmail.com>
2022-08-25 15:33:14 +0300

    Add hw3/Bird

--- src/hw3/Bird.java
+++ src/hw3/Bird.java
@@ -1,2 +1,31 @@
-package hw3;public class Bird {
+package hw3;
+
+public class Bird extends Object{
+    public Bird() {
+        super();
+    }
+
+    private String privateStaticMethod() {
+        return "From static Bird";
+    }
+
+    public static String publicStaticMethod() {
+        return "From public static Bird";
+    }
+
+    private String privateMethod() {
+        return "From private Bird";
+    }
+
+    String defaultMethod() {
+        return "From default Bird";
+    }
+
+    protected String protectMethod() {
+        return "From protected Bird";
+    }
+
+    public String publicMethod() {
+        return "From public Bird";
+    }
 }
