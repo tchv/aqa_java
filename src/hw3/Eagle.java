24ae34e44aafe81a6485e141d8a35f4b8c2b7abb
Taras Chornyi <taras.chornyi.v@gmail.com>
2022-08-26 13:21:15 +0300

    Add hw3/Eagle

--- src/hw3/Eagle.java
+++ src/hw3/Eagle.java
@@ -1,2 +1,10 @@
-package hw3;public class Eagle {
+package hw3;
+
+public class Eagle extends FlyingBird {
+    Eagle() {
+    }
+
+    Eagle(String feathers, String layEggs) {
+        super(feathers, layEggs);
+    }
 }
