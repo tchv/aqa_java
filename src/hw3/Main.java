6537fb66d93c029909098df75e5e0f69714f88f6
Taras Chornyi <taras.chornyi.v@gmail.com>
2022-08-26 13:22:35 +0300

    Add hw3/Main

--- src/hw3/Main.java
+++ src/hw3/Main.java
@@ -1,2 +1,22 @@
-package hw3;public class Main {
+package hw3;
+
+public class Main {
+    public static void main(String[] args) {
+        Eagle eagle = new Eagle("Brown", "No");
+        Swallow swallow = new Swallow("Blue", "No");
+        Penguin penguin = new Penguin("Red", "Yes");
+        Chicken chicken = new Chicken("Green", "Yes");
+
+        System.out.println(eagle.toString());
+        eagle.fly();
+
+        System.out.println(swallow.toString());
+        swallow.fly();
+
+        System.out.println(penguin.toString());
+        penguin.fly();
+
+        System.out.println(chicken.toString());
+        chicken.fly();
+    }
 }
