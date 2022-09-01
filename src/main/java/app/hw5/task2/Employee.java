178648ae329f09d9ba42f548c821bba4068eb1a2
Taras Chornyi <taras.chornyi.v@gmail.com>
2022-09-01 14:59:01 +0300

    Add hw5/task2/Employee.java

--- src/main/java/app/hw5/task2/Employee.java
+++ src/main/java/app/hw5/task2/Employee.java
@@ -1,17 +1,38 @@
 package app.hw5.task2;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Employee {
     public static void main(String[] args) {
-        int givenEmpId = 3;
-        ArrayList<emp> empList = new ArrayList<emp>();
-        empList.add(new emp(1, "hussain1"));
-        empList.add(new emp(2, "hussain2"));
-        empList.add(new emp(3, "hussain3"));
-        for (emp currEmp : empList) {
-            if (currEmp.id == givenEmpId) {
-                System.out.println("emp name is===>>" + currEmp.name);
+        String[] department = {"Company",
+                "1. Department of Tourism",
+                "2. Department of Ecology",
+                "3. Department of Sports"
+        };
+
+        Scanner sc = new Scanner(System.in);
+        int givenDepartmentNumber;
+
+        System.out.println("\nThe company has three departments:\n\t" +
+                "1. Department of Tourism;\n\t" +
+                "2. Department of Ecology;\n\t" +
+                "3. Department of Sports."
+        );
+        System.out.println("\nEnter department number:");
+        givenDepartmentNumber = sc.nextInt();
+        System.out.println(">>> " + department[givenDepartmentNumber] + "\n");
+
+        ArrayList<Data> employeeList = new ArrayList<>();
+        employeeList.add(new Data(1, "John Doe", 2500));
+        employeeList.add(new Data(2, "Harry Potter", 3000));
+        employeeList.add(new Data(2, "Sherlock Holmes", 3500));
+        employeeList.add(new Data(3, "James Bond", 2500));
+        employeeList.add(new Data(3, "Iron Man", 3000));
+        employeeList.add(new Data(3, "Tony Soprano", 3500));
+        for (Data currentEmployee : employeeList) {
+            if (currentEmployee.department_number == givenDepartmentNumber) {
+                System.out.println("Employee: " + currentEmployee.name + ", $" + currentEmployee.salary);
             }
         }
     }
