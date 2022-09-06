e0b3e39c31da971035bc3af675f798b3a35202d9
Taras Chornyi <taras.chornyi.v@gmail.com>
2022-09-06 14:09:14 +0300

    Add HW6 - class NewCollection

--- src/main/java/app/hw6/task1/NewCollection.java
+++ src/main/java/app/hw6/task1/NewCollection.java
@@ -1,19 +1,15 @@
 package app.hw6.task1;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
-
 
 public class NewCollection {
     static void newCollection() {
         ArrayList<Integer> list = new ArrayList<Integer>();
-
-        int min = -5;
-        int max = 25;
-
-        for (int i = min; i <= max; i++) {
-            list.add(i);
+        Random random = new Random();
+        for (int i = 0; i < 10; i++) {
+            Integer num = random.nextInt() % 25;
+            list.add(num);
         }
 
         List<Integer> newList = list.stream().filter(i -> i >= 5).sorted().collect(Collectors.toList());
