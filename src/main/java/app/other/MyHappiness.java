0eef72a15e06c69e940461d84e3b7654cafa09ea
Taras Chornyi <taras.chornyi.v@gmail.com>
2022-09-08 17:33:21 +0300

    Update Other

---
+++ src/main/java/app/other/MyHappiness.java
@@ -0,0 +1,17 @@
+package app.other.first;
+
+public class MyHappiness {
+    public static void main(String[] args) {
+        char[] s = {'С', 'р', 'а', 'к', 'а'};
+        String str = new String(s);
+        System.out.println("Було  : " + str);
+        String rStr = str.replace("Срака", "Щастя");
+        System.out.println("Стало : " + rStr);
+        if (str.equals("Срака")) {
+            System.out.println("Вітаю, у вас Щастя!");
+        } else {
+            System.out.println("Перевірте слово Срака");
+        }
+    }
+}
+
