d59c63aa7a5c3a487983f192f1be019e8d3048cb
Taras Chornyi <taras.chornyi.v@gmail.com>
2022-09-13 14:44:05 +0300

    Update MyHappiness

--- src/main/java/app/other/MyHappiness.java
+++ src/main/java/app/other/MyHappiness.java
@@ -3,10 +3,13 @@ package app.other;
 public class MyHappiness {
     public static void main(String[] args) {
         char[] s = {'С', 'р', 'а', 'к', 'а'};
+
         String str = new String(s);
         System.out.println("Було  : " + str);
+
         String rStr = str.replace("Срака", "Щастя");
         System.out.println("Стало : " + rStr);
+
         if (str.equals("Срака")) {
             System.out.println("Вітаю, у вас Щастя!");
         } else {
