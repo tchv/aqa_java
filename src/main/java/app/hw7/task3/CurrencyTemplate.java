37454bda4e59533687f73066ddef357ea2a83ad9
Taras Chornyi <taras.chornyi.v@gmail.com>
2022-09-13 14:46:16 +0300

    Update CurrencyTemplate

--- src/main/java/app/hw7/task3/CurrencyTemplate.java
+++ src/main/java/app/hw7/task3/CurrencyTemplate.java
@@ -1,5 +1,18 @@
 package app.hw7.task3;
 
-public class СurrencyТemplate {
+import java.util.*;
 
+public class CurrencyTemplate {
+    public static void main(String[] args) {
+        Formatter currencyFormat = new Formatter();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("\nPlease enter the dollar rate:");
+        double currency = sc.nextDouble();
+
+        String currencySymbol = "$";
+
+        currencyFormat.format("%, .2f", currency);
+        System.out.println(currencySymbol.concat(String.valueOf(currencyFormat)));
+    }
 }
