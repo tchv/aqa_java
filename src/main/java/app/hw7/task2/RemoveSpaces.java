1d79cf4cf0ceb5b9361e3386ed89e199ae888a70
Taras Chornyi <taras.chornyi.v@gmail.com>
2022-09-13 14:45:36 +0300

    Update RemoveSpaces

--- src/main/java/app/hw7/task2/RemoveSpaces.java
+++ src/main/java/app/hw7/task2/RemoveSpaces.java
@@ -1,2 +1,8 @@
-package app.hw7.task2;public class RemoveSpaces {
+package app.hw7.task2;
+
+public class RemoveSpaces {
+    public static void main(String[] args) {
+        String str = "I    am      learning     Java   Core";
+        System.out.println(str.replaceAll("\\s+"," ").trim());
+    }
 }
