1093607231a24cba0cbae1acc7b249145f63eb22
Taras Chornyi <taras.chornyi.v@gmail.com>
2022-09-13 14:44:59 +0300

    Update FiveWords

--- src/main/java/app/hw7/task1/FiveWords.java
+++ src/main/java/app/hw7/task1/FiveWords.java
@@ -1,5 +1,15 @@
 package app.hw7.task1;
 
-public class FiveWorbs {
-    
+public class FiveWords {
+    public static void main( String[] args ) {
+        String s = "I love coffee in the morning";
+        String[] word = s.split(" ");
+        String maxWord = "";
+        for (String value : word) {
+            if (value.length() >= maxWord.length()) {
+                maxWord = value;
+            }
+        }
+        System.out.println("The biggest word in the sentence: '" + maxWord + "' - " + maxWord.length() + " letters");
+    }
 }
