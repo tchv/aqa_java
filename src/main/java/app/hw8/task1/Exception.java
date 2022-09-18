d16915f6fb9fb600d1f5ca28e9e37ce884779e00
Taras Chornyi <taras.chornyi.v@gmail.com>
2022-09-18 14:47:39 +0300

    HW8 add Exception

--- src/main/java/app/hw8/task1/Exception.java
+++ src/main/java/app/hw8/task1/Exception.java
@@ -3,7 +3,7 @@ package app.hw8.task1;
 public class Exception {
     public static int squareRectangle(int a, int b){
         if (a < 0 || b < 0){
-            throw new RuntimeException("error");
+            throw new RuntimeException("You entered a negative value!");
         }
         return a * b;
     }
