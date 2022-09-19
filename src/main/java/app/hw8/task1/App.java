1a47b1c4f81ce6e095052b3b66264bf135cc66b0
Taras Chornyi <taras.chornyi.v@gmail.com>
2022-09-19 15:38:27 +0300

    HW8 update App

--- src/main/java/app/hw8/task1/App.java
+++ src/main/java/app/hw8/task1/App.java
@@ -3,25 +3,34 @@ package app.hw8.task1;
 import java.util.Scanner;
 
 public class App {
-    private static int squareRectangleValues() {
+    private static String getFromConsole(String msg) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter the value of side A:");
-        String valueA = sc.nextLine();
-        System.out.println(">>> " + valueA);
+        System.out.println(msg);
+        String value = sc.nextLine();
+        System.out.println(">>> " + value);
 
-        System.out.println("Enter the value of side B:");
-        String valueB = sc.nextLine();
-        System.out.println(">>> " + valueB);
-
+        return value;
+    }
+    private static String getSquare() {
+        String valueA = getFromConsole("Enter the value of side A:");
         int a = Integer.parseInt(valueA);
+
+        String valueB = getFromConsole("Enter the value of side B:");
         int b = Integer.parseInt(valueB);
 
-        return Exception.squareRectangle(a, b);
+        try {
+            int sq = Rectangle.squareRectangle(a, b);
+            return String.valueOf(sq);
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return "Can not calculate";
     }
 
     public static void main(String[] args) {
         System.out.println("\n[ AREA CALCULATOR ]\n");
-        System.out.println("Result: Area = ".concat(String.valueOf(squareRectangleValues())));
+        System.out.println("Result: Area = " + getSquare());
     }
 }
--- src/main/java/app/hw8/task1/Rectangle.java
+++ src/main/java/app/hw8/task1/Rectangle.java
@@ -1,6 +1,6 @@
 package app.hw8.task1;
 
-public class Exception {
+public class Rectangle {
     public static int squareRectangle(int a, int b){
         if (a < 0 || b < 0){
             throw new RuntimeException("You entered a negative value!");
